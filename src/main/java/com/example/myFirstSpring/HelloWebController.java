5fd4d0935a738b9eec5421b67dc01a0228603402
Japoivvb <jose.portugal.ortuno@gmail.com>
2023-10-15 19:20:37 +0200

    send get param

--- src/main/java/com/example/myFirstSpring/HelloWebController.java
+++ src/main/java/com/example/myFirstSpring/HelloWebController.java
@@ -2,7 +2,10 @@ package com.example.myFirstSpring;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
@@ -11,5 +14,16 @@ public class HelloWebController {
     public String helloHandler(){
 
         return "welcome.html";
+    }
+    
+    
+    @GetMapping("/helloname")
+    public String helloParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") 
+			String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
     }
 }
